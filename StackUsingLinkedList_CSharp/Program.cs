aeec4d08b8dbd5e3bce1b546f42c7f89067ee598
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-12-02 15:50:14 +0700

    create class Node

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StackUsingLinkedList_CSharp
 {
-    internal class Program
+    class Node
     {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
         static void Main(string[] args)
         {
         }
